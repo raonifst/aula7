08f92baf97d4dca63a2d8551bccbafab4a9c3756
Raoni Florentino da Silva Teixeira <raoniteixeira@gmail.com>
2021-03-23 18:49:51 +0000

    Initial commit

---
+++ mostrar_numero_2.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    i = 1;
+    
+    printf("%d.\n", i);
+    i = i + 1;
+    
+    printf("%d.\n", i);
+    i = i + 1;
+    
+    printf("%d.\n", i);
+    i = i + 1;
+    
+    printf("%d.\n", i);
+    i = i + 1;
+    
+    printf("%d.\n", i);
+    i = i + 1;
+    
+    printf("%d.\n", i);
+    i = i + 1;
+    
+    printf("%d.\n", i);
+    i = i + 1;
+    
+    printf("%d.\n", i);
+    i = i + 1;
+    
+    printf("%d.\n", i);
+    i = i + 1;
+    
+    printf("%d.\n", i);
+    i = i + 1;
+    
+    
+    return 0;
+}
+
+
